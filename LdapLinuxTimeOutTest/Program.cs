51a4f3b45fbd57ba74f9707e829abce6be6dffd8
Tomasovic Martin <martin_tomasovic@kb.cz>
2023-02-21 13:23:30 +0100

    Add project files.

---
+++ LdapLinuxTimeOutTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using System.DirectoryServices.Protocols;
+using System.Net;
+using System.Reflection;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+    .Build();
+
+var setttings = configuration.GetSection("ldap");
+
+var connection = new LdapConnection(
+      new LdapDirectoryIdentifier(setttings.GetValue<string>("serverName"), setttings.GetValue<int>("serverPort")),
+      new NetworkCredential(setttings.GetValue<string>("userName"), setttings.GetValue<string>("Password"))
+      , AuthType.Basic);
+connection.SessionOptions.ReferralChasing = ReferralChasingOptions.None;
+connection.SessionOptions.SecureSocketLayer = setttings.GetValue<bool>("useSSL");
+connection.SessionOptions.ProtocolVersion = 3;
+connection.Bind();
+var distinguishedName = setttings.GetValue<string>("domain").Split('.').Select(name => $"dc={name}").Aggregate((a, b) => $"{a},{b}");
+
+while (true)
+{
+  var filter = $"(&(objectClass=user)(sAMAccountName=testUser))";
+  var searchRequest = new SearchRequest(distinguishedName, filter, SearchScope.Subtree);
+
+  Debug.Assert(connection != null);
+  var searchResponse = (SearchResponse)await Task<DirectoryResponse>.Factory.FromAsync(
+      connection.BeginSendRequest!,
+      connection.EndSendRequest,
+      searchRequest,
+      PartialResultProcessing.NoPartialResultSupport,
+      null);
+
+  Console.WriteLine($"Search completed, results count {searchResponse.Entries.Count}");
+
+  await Task.Delay(TimeSpan.FromMinutes(16));
+}
